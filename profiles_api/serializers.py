02a73641efe4cf6c80da4203d61c16f70523f5bd
Sepehr Nematollahi <nematollahisepehr@gmail.com>
2020-08-29 16:32:18 +0430

    Final commit

---
+++ profiles_api/serializers.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+from profiles_api import models
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        user = models.UserProfile.objects.create_user(
+            email=validated_data.get('email'),
+            name=validated_data.get('name'),
+            password=validated_data.get('password')
+        )
+
+        return user
+
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}}
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework import filters
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.settings import api_settings
 
-# Create your views here.
+from profiles_api import models
+from profiles_api import serializers, permissions
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticatedOrReadOnly
+    )
+
+    def perform_create(self, serializer):
+        serializer.save(user_profile=self.request.user)
